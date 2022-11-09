57aafc8c26bb3e3b27658ba5829963a3f175aff4
Elias Joa <eliasij@berkeley.edu>
2022-11-09 10:26:23 -0800

    Finished storefile for now

--- client/client.go
+++ client/client.go
@@ -108,45 +108,45 @@ type User struct {
 	Signature_private_key userlib.DSSignKey
 	master_key            []byte
 	hmac_key              []byte
-	Files_owned           map[string]int
+	Files_owned           map[string]bool
 }
 
 type File struct {
 	Content   []byte
-	Next_uuid uuid.UUID
+	Next_uuid uuid.UUID //UUID of the next file in list after this one
 }
 
 type FileController struct {
-	Start uuid.UUID
-	End   uuid.UUID
+	Start uuid.UUID //UUID of first file
+	End   uuid.UUID //UUID of end of file
 }
 
 type FileReferenceOwner struct {
-	Uuid_shared_with      map[string]uuid.UUID
-	Enc_keys_shared_with  map[string][]byte
-	Hmac_keys_shared_with map[string][]byte
-	File_enc_key          []byte
-	Hmac_key              []byte
-	Uuid_file_reference   uuid.UUID
+	Uuid_shared_with        map[string]uuid.UUID
+	Enc_keys_shared_with    map[string][]byte
+	Hmac_keys_shared_with   map[string][]byte
+	File_enc_key            []byte
+	Hmac_key                []byte
+	File_controller_pointer uuid.UUID //UUID for file controller
 }
 
 type FileReferencePrimary struct {
-	File_enc_key []byte
-	Hmac_key     []byte
-	File_pointer uuid.UUID
+	File_enc_key            []byte
+	Hmac_key                []byte
+	File_controller_pointer uuid.UUID
 }
 
 type FileReferenceSecondary struct {
-	File_Reference_Primary_enc_key        []byte
-	Hmac_key                              []byte
-	HMAC                                  []byte
-	Uuid_file_reference_primary_reference uuid.UUID
+	File_Reference_Primary_enc_key []byte
+	Hmac_key                       []byte
+	HMAC                           []byte
+	File_reference_primary_pointer uuid.UUID
 }
 
 type Invitation struct {
 	FileReferencePrimary_decrypt_key []byte
 	FileReferencePrimary_hmac_key    []byte
-	FileReferencePrimary_reference   uuid.UUID
+	FileReferencePrimary_pointer     uuid.UUID
 }
 
 // You can add other attributes here if you want! But note that in order for attributes to
@@ -341,10 +341,23 @@ func (userdata *User) StoreFile(filename string, content []byte) (err error) {
 	//Check if file exists
 	datastore_file_content, ok := userlib.DatastoreGet(file_uuid)
 
+	//Create the encryption key
+	encryption_key_64, err := userlib.HashKDF(userdata.master_key, []byte("Encryption key for file"+filename))
+	if err != nil {
+		return err
+	}
+	encryption_key := encryption_key_64[:16]
+	//Create the HMAC key
+	hmac_key_64, err := userlib.HashKDF(userdata.master_key, []byte("HMAC key for file"+filename))
+	if err != nil {
+		return err
+	}
+	hmac_key := hmac_key_64[:16]
+
 	//If the file does not exist
 	if !ok {
 		//Update the userdata with the new file owned
-		userdata.Files_owned[filename] = 1
+		userdata.Files_owned[filename] = true
 		//Upload the new userdata to the datastore using helper function
 		err := UploadUserdata(userdata)
 		if err != nil {
@@ -359,26 +372,13 @@ func (userdata *User) StoreFile(filename string, content []byte) (err error) {
 		file_reference_owner.File_enc_key = userlib.RandomBytes(16)
 		file_reference_owner.Hmac_key = userlib.RandomBytes(16)
 		//The new file's UUID
-		file_reference_owner.Uuid_file_reference = uuid.New()
+		file_reference_owner.File_controller_pointer = uuid.New()
 
 		//The sharing functionality
 		//Create a map for sharing of the UUID, HMAC_keys and Encryption keys to people with access
 		file_reference_owner.Uuid_shared_with = make(map[string]uuid.UUID)
 		file_reference_owner.Hmac_keys_shared_with = make(map[string][]byte)
 		file_reference_owner.Enc_keys_shared_with = make(map[string][]byte)
-
-		//Create the encryption key for filereferenceowner
-		encryption_key_64, err := userlib.HashKDF(userdata.master_key, []byte("Encryption key for file"+filename))
-		if err != nil {
-			return err
-		}
-		encryption_key := encryption_key_64[:16]
-		//Create the HMAC key for filereferenceowner
-		hmac_key_64, err := userlib.HashKDF(userdata.master_key, []byte("HMAC key for file"+filename))
-		if err != nil {
-			return err
-		}
-		hmac_key := hmac_key_64[:16]
 
 		//Turn filereferenceowner to bytes and encrypt and then mac
 		file_reference_owner_bytes, err := json.Marshal(file_reference_owner)
@@ -428,7 +428,7 @@ func (userdata *User) StoreFile(filename string, content []byte) (err error) {
 		}
 		file_controller_bytes_encrypted_HMAC := append(file_controller_bytes_encrypted, file_controller_bytes_HMAC...)
 		//Store in datastore
-		userlib.DatastoreSet(file_reference_owner.Uuid_file_reference, file_controller_bytes_encrypted_HMAC)
+		userlib.DatastoreSet(file_reference_owner.File_controller_pointer, file_controller_bytes_encrypted_HMAC)
 
 		//Now we store the file at the UUID referenced by file_controller.Start
 		//Marshall it
@@ -469,7 +469,203 @@ func (userdata *User) StoreFile(filename string, content []byte) (err error) {
 		userlib.DatastoreSet(file_controller.End, empty_file_bytes_encrypted_HMAC)
 
 		return nil
+
+		//For when the file already exists
+		//If the user owns the file
+		owns_file := userdata.Files_owned[filename]
+		if owns_file {
+			var file_reference_owner FileReferenceOwner
+			var file_controller FileController
+
+			//We want to now access the filereference owner to see where our filecontroller is
+			//Need to first check integrity of filereferenceowner using valid mac helper function
+			if !ValidHMAC(hmac_key, datastore_file_content) {
+				return errors.New("The integrity of the FileReferenceOwner of this file has been compromised")
+			}
+			//As it is integral we can load the filereference owner from datastore_file_content
+			datastore_file_content_encrypted := datastore_file_content[:len(datastore_file_content)-64]
+			//Decrypt it
+			datastore_file_content_decrypted := userlib.SymDec(encryption_key, datastore_file_content_encrypted)
+			//Unmarshal it
+			err := json.Unmarshal(datastore_file_content_decrypted, &file_reference_owner)
+			if err != nil {
+				return err
+			}
+			//Load the file controller
+			file_controller_bytes_with_HMAC, ok := userlib.DatastoreGet(file_reference_owner.File_controller_pointer)
+			if !ok {
+				return errors.New("Something wrong with accessing file controller")
+			}
+			//Check HMAC of file controller
+			if !ValidHMAC(file_reference_owner.Hmac_key, file_controller_bytes_with_HMAC) {
+				return errors.New("The integrity of the file controller is compromised")
+			}
+			//Decrypt it
+			file_controller_bytes_encrypted := file_controller_bytes_with_HMAC[:len(file_controller_bytes_with_HMAC)-64]
+			file_controller_bytes_decrypted := userlib.SymDec(file_reference_owner.File_enc_key, file_controller_bytes_encrypted)
+			//Demarshal it
+			err = json.Unmarshal(file_controller_bytes_decrypted, &file_controller)
+			if err != nil {
+				return err
+			}
+
+			//Now we know where the start and end of the linked file is
+			//The idea is to create a new file and update the start attribute in the file controller
+			//The empty end of the previous file list can be discarded as there are no requirement that this should be deleted
+
+			//Now we create the new start and end to the linked list of files
+			var start_file File
+			var end_file File
+			start_file.Content = content
+			//Add the next file as a random new uuid
+			start_file.Next_uuid = uuid.New()
+			//Update the file_controller with the new tail
+			file_controller.End = start_file.Next_uuid
+			//Store the new file at the beginning of the list as told in the file_controller.start
+			//Marshal
+			start_file_bytes, err := json.Marshal(start_file)
+			if err != nil {
+				return err
+			}
+			//Encrypt
+			iv = userlib.RandomBytes(16)
+			start_file_bytes_encrypted := userlib.SymEnc(file_reference_owner.File_enc_key, iv, start_file_bytes)
+			//HMAC
+			start_file_bytes_HMAC, err := userlib.HMACEval(file_reference_owner.Hmac_key, start_file_bytes_encrypted)
+			if err != nil {
+				return err
+			}
+			start_file_bytes_encrypted_HMAC := append(start_file_bytes_encrypted, start_file_bytes_HMAC...)
+			//Store it at file_controller_start
+			userlib.DatastoreSet(file_controller.Start, start_file_bytes_encrypted_HMAC)
+
+			//Do the same for the empty tail
+			//Marshal
+			end_file_bytes, err := json.Marshal(end_file)
+			if err != nil {
+				return err
+			}
+			//Encrypt
+			iv = userlib.RandomBytes(16)
+			end_file_bytes_encrypted := userlib.SymEnc(file_reference_owner.File_enc_key, iv, end_file_bytes)
+			//HMAC
+			end_file_bytes_HMAC, err := userlib.HMACEval(file_reference_owner.Hmac_key, end_file_bytes_encrypted)
+			if err != nil {
+				return err
+			}
+			end_file_bytes_encrypted_HMAC := append(end_file_bytes_encrypted, end_file_bytes_HMAC...)
+			//Store it at file_controller_end or file_starts next uuid
+			userlib.DatastoreSet(start_file.Next_uuid, end_file_bytes_encrypted_HMAC)
+
+			return nil
+		}
+		//If the user does not own the file
+		//We now take advantage of our filereferenceprimary and filereferencesecondary structs
+		var file_reference_primary FileReferencePrimary
+		var file_reference_secondary FileReferenceSecondary
+
+		//We first open the filereferencesecondary file
+		//This would be at the UUID inputted when calling store file
+		//That is: FromBytes(username+password+filename)
+		//This is already loaded
+		//Check HMAC
+		if !ValidHMAC(hmac_key, datastore_file_content) {
+			return errors.New("Integrity of the filereferencesecondary is compromised")
+		}
+		//If HMAC is valid decrypt and then demarshal
+		//Decrypt using the key used to encrypt the filereferencesecondary upon sharing
+		datastore_file_content_encrypted := datastore_file_content[:len(datastore_file_content)-64]
+		datastore_file_content_decrypted := userlib.SymDec(encryption_key, datastore_file_content_encrypted)
+		//Demarshal
+		err = json.Unmarshal(datastore_file_content_decrypted, &file_reference_secondary)
+		if err != nil {
+			return err
+		}
+
+		//Now we can open the filereferenceprimary from the filereferencesecondary
+		file_reference_primary_bytes_encrypted_HMAC, ok := userlib.DatastoreGet(file_reference_secondary.File_reference_primary_pointer)
+		if !ok {
+			return errors.New("Not able to retrieve the filereferenceprimary file from filereferencesecondary file")
+		}
+		//Check the HMCA of the file_reference_primary
+		if !ValidHMAC(file_reference_secondary.Hmac_key, file_reference_primary_bytes_encrypted_HMAC) {
+			return errors.New("The integrity of the filereferenceprimary is compromised")
+		}
+		//Decrypt it
+		file_reference_primary_bytes_encrypted := file_reference_primary_bytes_encrypted_HMAC[:len(file_reference_primary_bytes_encrypted_HMAC)-64]
+		file_reference_primary_bytes_decrypted := userlib.SymDec(file_reference_secondary.File_Reference_Primary_enc_key, file_reference_primary_bytes_encrypted)
+		//Unmarshal
+		err = json.Unmarshal(file_reference_primary_bytes_decrypted, &file_reference_primary)
+		if err != nil {
+			return err
+		}
+		//We now have access to the filereferenceprimary which gives access to the file controller and the encryption key and hmac key
+		//We can now load the file controller
+		file_controller_bytes_encrypted_HMAC, ok = userlib.DatastoreGet(file_reference_primary.File_controller_pointer)
+		if !ok {
+			return errors.New("Could not retrieve file controller from filereferenceprimary")
+		}
+		//Check the HMAC
+		if !ValidHMAC(file_reference_primary.Hmac_key, file_controller_bytes_encrypted_HMAC) {
+			return errors.New("The integrity of the file controller has been compromised")
+		}
+		//Decrypt
+		file_controller_bytes_encrypted = file_controller_bytes_encrypted_HMAC[:len(file_controller_bytes_encrypted_HMAC)-64]
+		file_controller_bytes_decrypted := userlib.SymDec(file_reference_primary.File_enc_key, file_controller_bytes_encrypted)
+		//Unmarshal
+		err = json.Unmarshal(file_controller_bytes_decrypted, &file_controller)
+		if err != nil {
+			return err
+		}
+
+		//Now we have access to the file controller and can store the new content
+		var start_file File
+		var end_file File
+		start_file.Content = content
+		start_file.Next_uuid = uuid.New()
+		//Update the end of the file list
+		file_controller.End = start_file.Next_uuid
+
+		//Store the start file
+		//Marshal
+		start_file_bytes, err := json.Marshal(start_file)
+		if err != nil {
+			return err
+		}
+		//Encrypt
+		iv = userlib.RandomBytes(16)
+		start_file_bytes_encrypted := userlib.SymEnc(file_reference_primary.File_enc_key, iv, start_file_bytes)
+		//Add HMAC
+		start_file_bytes_HMAC, err := userlib.HMACEval(file_reference_primary.Hmac_key, start_file_bytes_encrypted)
+		if err != nil {
+			return err
+		}
+		start_file_bytes_encrypted_HMAC := append(start_file_bytes_encrypted, start_file_bytes_HMAC...)
+		//Store in datastore
+		userlib.DatastoreSet(file_controller.Start, start_file_bytes_encrypted_HMAC)
+
+		//Do the same for the empty tail
+		//Marshal
+		end_file_bytes, err := json.Marshal(end_file)
+		if err != nil {
+			return err
+		}
+		//Encrypt
+		iv = userlib.RandomBytes(16)
+		end_file_bytes_encrypted := userlib.SymEnc(file_reference_primary.File_enc_key, iv, end_file_bytes)
+		//HMAC
+		end_file_bytes_HMAC, err := userlib.HMACEval(file_reference_primary.Hmac_key, end_file_bytes_encrypted)
+		if err != nil {
+			return err
+		}
+		end_file_bytes_encrypted_HMAC := append(end_file_bytes_encrypted, end_file_bytes_HMAC...)
+		//Store it at file_controller_end or file_starts next uuid
+		userlib.DatastoreSet(start_file.Next_uuid, end_file_bytes_encrypted_HMAC)
+
+		return nil
 	}
+
+	//Might have to reupload file controller to datastore, not sure yet
 
 	storageKey, err := uuid.FromBytes(userlib.Hash([]byte(filename + userdata.Username))[:16])
 	if err != nil {
@@ -544,4 +740,16 @@ func UploadUserdata(userdata *User) (err error) {
 	userlib.DatastoreSet(user_UUID, user_bytes_encrypted_MAC)
 
 	return nil
+}
+
+func ValidHMAC(hmac_key []byte, content_with_HMAC []byte) (valid bool) {
+	hmac := content_with_HMAC[len(content_with_HMAC)-64:]
+	content := content_with_HMAC[0 : len(content_with_HMAC)-64]
+
+	hmac_content, err := userlib.HMACEval(hmac_key, content)
+	if err != nil {
+		return false
+	}
+	valid = userlib.HMACEqual(hmac, hmac_content)
+	return valid
 }
